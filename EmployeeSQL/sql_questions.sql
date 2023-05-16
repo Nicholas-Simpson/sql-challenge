ccaa9cad3f67a4f19cb55ca6e061604696571dde
Nicholas Simpson <114123854+Nicholas-Simpson@users.noreply.github.com>
2023-05-15 18:22:39 -0700

    doing HW q2

--- EmployeeSQL/sql_questions.sql
+++ EmployeeSQL/sql_questions.sql
@@ -18,5 +18,25 @@ SELECT
 	,hire_date
 FROM
 	employees
-	where hire_date > 1986
+	where hire_date between '1986-01-01' and '1986-12-31' 
+	ORDER BY hire_date ASC
 ;
+
+/* Question 3 List the manager of each department along with their department number, department name, employee number, last name, and first name. */
+SELECT
+	d.dept_no
+	,d.dept_name
+	,de.emp_no
+	,e.last_name
+	,e.first_name
+FROM
+	departments as d
+	inner join dept_emp as de on de.dept_no = d.dept_no
+	inner join employees as e on e.emp_no = de.emp_no
+;
+
+
+
+
+
+
